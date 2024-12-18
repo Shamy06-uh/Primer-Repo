d9100575b9b94675f2adf45b9ca6b49e47745e67
Shamy06-uh <maristanyshamara06@gmail.com>
2024-12-18 13:45:18 -0500

    Added main method to program

--- Program.cs
+++ Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Name
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
